5e65182140450d1f55c57047115650e83aa16ad5
Omar Atef Shokry <216340749+Omar12Atef@users.noreply.github.com>
2026-07-21 23:57:05 +0300

    Add name encryption and decryption functionality
    Implement functions for name encryption and decryption.

---
+++ 05-Algorithms-L2/Problem_18.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+string ReadName()
+{
+    string Name ;
+
+    cout << "Enter Your Name?\n" ;
+    getline(cin , Name) ;
+    return Name ;
+}
+
+string Encryption(string Name , short EncryptionKey)
+{
+    for (int i=0 ; i< Name.length() ; i++)
+    {
+        Name[i] = char (Name[i] + EncryptionKey) ;
+    }
+
+    return Name ;
+}
+
+string Decryption(string Name , short EncryptionKey)
+{
+    for (int i=0 ; i< Name.length() ; i++)
+    {
+        Name[i] = char (Name[i] - EncryptionKey) ;
+    }
+
+    return Name ;
+}
+
+int main() 
+{
+    const short EncryptionKey = 2 ;
+
+    string Name = ReadName() ;
+    string EncryptionName = Encryption(Name , EncryptionKey) ;
+    string DecryptionName = Decryption(EncryptionName , EncryptionKey) ;
+
+    cout << "\nName Before Encryption : " << Name << endl ;
+    cout << "Name After Encryption : " << EncryptionName << endl ;
+    cout << "Name After Decryption : " << DecryptionName << endl ;
+
+    return 0;
+}
