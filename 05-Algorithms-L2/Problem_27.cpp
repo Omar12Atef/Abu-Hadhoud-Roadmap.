b1d3d1544232941fbd121939e89a01b274194d92
Omar Atef Shokry <216340749+Omar12Atef@users.noreply.github.com>
2026-07-23 16:16:53 +0300

    Average of array's Numbers.

---
+++ 05-Algorithms-L2/Problem_27.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+int RandomNumber (int From , int To)
+{
+    int random = rand() % (To - From + 1) + From ;
+    return random ;
+}
+
+
+int ReadPositiveNumber(string Message)
+{
+    int Number ;
+
+    do
+    {
+        cout << Message << endl ;
+        cin >> Number ;
+    } while (Number <= 0) ;
+
+    return Number ;
+}
+
+void FillArrayWithRandomNumbers(int arr[100] , int ArrLength)
+{
+    for (int i = 0 ; i < ArrLength ; i++)
+    {
+        arr[i] = RandomNumber(1 , 100) ;
+    }
+}
+
+int SumArray(int arr[100] , int ArrLength)
+{
+
+    int sum = 0 ;
+    for (int i = 0 ; i < ArrLength ; i++)
+    {
+        sum = sum + arr[i] ;
+    }
+
+    return sum ;
+}
+
+int AverageOfArrayElements (int arr[100] , int ArrLength)
+{
+    return (float) SumArray(arr , ArrLength) / ArrLength ;
+}
+
+
+void PrintArray(int arr[100] , int ArrLength)
+{
+    for (int i = 0 ; i < ArrLength ; i++)
+    {
+        
+        cout << arr[i] << " " ;
+    }
+}
+
+
+int main() 
+{
+
+    srand((unsigned)time(NULL)) ;
+
+    int arr[100] , ArrLength ;
+
+    ArrLength = ReadPositiveNumber("Enter The length of array : ") ;
+    FillArrayWithRandomNumbers(arr , ArrLength) ;
+
+    cout << "Array Elements : " ;
+    PrintArray(arr , ArrLength) ;
+
+    cout << "\n\nAverage of All Numbers is : " << AverageOfArrayElements(arr , ArrLength) ;
+
+
+
+    return 0;
+}
