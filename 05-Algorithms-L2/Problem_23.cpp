f758acacd667e457ca399779021b9e5a1a3667dc
Omar Atef Shokry <216340749+Omar12Atef@users.noreply.github.com>
2026-07-23 15:36:09 +0300

    Add Problem_23.cpp for random number generation

---
+++ 05-Algorithms-L2/Problem_23.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int RandomNumber (int From , int To)
+{
+    int random = rand() % (To - From + 1) + From ;
+    return random ;
+}
+
+
+int ReadPositiveNumber(string Message)
+{
+    int Number ;
+
+    do
+    {
+        cout << Message << endl ;
+        cin >> Number ;
+    } while (Number <= 0) ;
+
+    return Number ;
+}
+
+
+void PrintArray(int arr[100] , int ArrLength)
+{
+    for (int i = 0 ; i < ArrLength ; i++ )
+    {
+        arr[i] = RandomNumber(1 , 100) ;
+        cout << arr[i] << " " ;
+    }
+}
+
+
+int main() 
+{
+
+    srand((unsigned)time(NULL)) ;
+
+    int arr[100] , ArrLength ;
+
+    ArrLength = ReadPositiveNumber("Enter The length of array : ") ;
+
+    cout << "Array Elements : " ;
+    PrintArray(arr , ArrLength) ;
+
+
+
+    return 0;
+}
