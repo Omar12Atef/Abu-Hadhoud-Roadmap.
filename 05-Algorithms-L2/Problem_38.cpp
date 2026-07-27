6bef71c3e74b799e867665f5ec2bf56e847436f0
Omar Atef Shokry <216340749+Omar12Atef@users.noreply.github.com>
2026-07-27 23:15:00 +0300

    Add Problem_38.cpp for random number processing (Odd Numbers Copy.)

---
+++ 05-Algorithms-L2/Problem_38.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+int ReadPositiveNumber()
+{
+    int Number ;
+
+    do
+    {
+        cout << "ُEnter The Number of Elements : " ;
+        cin >> Number ;
+    } while (Number <= 0) ;
+
+    return Number ; 
+}
+
+int RandomNumber (int From , int To)
+{
+    int Random = rand() % (To - From + 1) + From ;
+
+    return Random ;
+}
+
+void FillArrayWithRandomNumbers(int arr[100] , int &arrLength)
+{
+    for (int i = 0 ; i < arrLength ; i++)
+    {
+        arr[i] = RandomNumber(1,100) ;
+    }
+}
+
+void AddArayElement(int Number , int arr[100] , int &arrLength)
+{
+    arrLength ++ ;
+    arr[arrLength - 1] = Number ;
+}
+
+void ChekOddNumbersInArray1(int arr1[100] , int arr2[100] , int arrLength , int &arr2length)
+{
+    for (int i = 0 ; i < arrLength ; i++)
+    {
+        if (arr1[i] % 2 != 0)
+        {
+            AddArayElement(arr1[i] , arr2 , arr2length) ;
+        }
+    }
+}
+
+void printArray (int arr[100] , int arrLength)
+{
+    for (int i = 0 ; i < arrLength ; i++)
+    {
+        cout << arr[i] << " " ;
+    }
+}
+
+int main() 
+{
+
+    srand((unsigned)time(NULL)) ;
+
+    int arr1[100] , arr2[100] , arrLength = 0 , arr2length = 0 ;
+
+    arrLength = ReadPositiveNumber() ;
+
+    FillArrayWithRandomNumbers(arr1 , arrLength) ;
+    ChekOddNumbersInArray1(arr1 , arr2 , arrLength , arr2length) ;
+
+    cout << "\nArray 1 Elements : \n" ;
+    printArray(arr1 , arrLength) ;
+
+    cout << "\n\nArray 2 Odd Elements : \n" ;
+    printArray(arr2 , arr2length) ;
+
+
+
+    return 0;
+}
