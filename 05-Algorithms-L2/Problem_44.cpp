2c68cb7da33165bebb0df5c8db1aace06d833cbf
Omar Atef Shokry <216340749+Omar12Atef@users.noreply.github.com>
2026-07-29 14:36:33 +0300

    Count Positive Numbers in Array.

---
+++ 05-Algorithms-L2/Problem_44.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int ReadNumber()
+{
+    int arrlength ;
+
+    cout << "Enter Array Length : " ;
+    cin >> arrlength ;
+
+    return arrlength ;
+}
+
+int RandomNumber(int From , int To)
+{
+    int random = rand() % (To - From + 1) + From ;
+    return random ;
+}
+
+void FillArrayWithRandomNumbers(int arr[100] , int &arrlength)
+{
+    for (int i = 0 ; i < arrlength ; i++)
+    {
+        arr[i] = RandomNumber(-100 , 100) ;
+    }
+}
+
+int PositiveNumbersCount(int arr[100] , int &arrlength)
+{
+    int counter = 0 ;
+
+    for (int i = 0 ; i < arrlength ; i++)
+    {
+        if (arr[i] > 0)
+        {
+            counter++ ;
+        }
+    }
+
+    return counter ;
+}
+
+void PrintArray(int arr[100] , int &arrlength)
+{
+    for (int i = 0 ; i < arrlength ; i++)
+    {
+        cout << arr[i] << " " ;
+    }
+}
+
+int main() 
+{
+    srand((unsigned)time(NULL)) ;
+
+    int arr[100] ;
+
+    int arrlength = ReadNumber() ;
+
+    FillArrayWithRandomNumbers(arr , arrlength) ;
+
+    cout << "\nArray Elements :\n" ;
+    PrintArray(arr , arrlength) ;
+
+    cout << "\n\nEven Numbers count is : " ;
+    cout << PositiveNumbersCount(arr , arrlength) ;
+
+    return 0;
+}
