7cdedd79ca369db32200c837b649c8fac0ecf488
Omar Atef Shokry <216340749+Omar12Atef@users.noreply.github.com>
2026-07-29 16:03:33 +0300

    My Floor.

--- 05-Algorithms-L2/Problem_48.cpp
+++ 05-Algorithms-L2/Problem_48.cpp
@@ -13,16 +13,25 @@ float ReadNumber()
     return Number ;
 }
 
+float GetFractionPart(float Number)
+{
+    return Number - int (Number) ;
+}
+
 int MyFloor(float Number)
 {
-    if (Number > 0)
+    int IntPart = int (Number) ;
+    float FractionPart = GetFractionPart(Number) ;
+
+    if (abs(FractionPart) > 0)
     {
-        return int(Number) ;
+        if (Number > 0)
+            return IntPart ;
+        else
+            return IntPart - 1 ;
     }
     else
-    {
-        return int(Number) - 1 ;
-    }
+        return IntPart ;
 }
 
 int main() 
